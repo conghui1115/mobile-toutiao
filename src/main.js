21e16e224ff3924e50600edd574d44224e30865d
conghui <740364177@qq.com>
2020-06-22 11:30:19 +0800

    导入vant组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 全部导入 vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant) // 全局注册  实际调用了vant  导出的对象的方法 install方法
 Vue.config.productionTip = false
 
 new Vue({
