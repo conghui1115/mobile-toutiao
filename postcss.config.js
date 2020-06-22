22f70ce377271aab10240f6f68e4db0fd2911bcf
conghui <740364177@qq.com>
2020-06-22 12:04:22 +0800

    amfe-felxible屏幕变自己大小变插件

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ module.exports = {
     autoprefixer: {},
     'postcss-pxtorem': {
       rootValue: 37.5, // 基准值 font-size: 30px  =>  300/ rootValue = rem
+      // rootValue 是转换px的基准值，参考设备iPhone6，设备宽度375px。
       propList: ['*'] // 包括哪些文件  所有的文件都转化rem
     }
   }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import store from './store'
 // 全部导入 vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
+
+// 屏幕变化，font-size变化，修改rem 基准值的js插件 amfe-flexible
+import 'amfe-flexible'
 Vue.use(Vant) // 全局注册  实际调用了vant  导出的对象的方法 install方法
 Vue.config.productionTip = false
 
