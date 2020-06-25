138f893feb16afd296ccc7ca464731b1473b018f
conghui <740364177@qq.com>
2020-06-25 20:32:08 +0800

    登录页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,17 @@
 <template>
 <div class="container">
     <van-nav-bar left-arrow title='登录' @click-left="$router.back()"></van-nav-bar>
+    <!-- 登录布局 -->
+    <van-cell-group>
+      <van-field label="手机号" placeholder="请输入手机号"></van-field>
+      <van-field label="验证码" placeholder="请输入验证码">
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+      <div class="login-box">
+        <van-button type="info" round  block>登录</van-button>
+      </div>
   </div>
 </template>
 
@@ -11,5 +22,7 @@ export default {
 </script>
 
 <style>
-
+.login-box{
+  padding: 20px;
+}
 </style>
